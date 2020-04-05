c25ad9deaaecffce5fad8ee026c3428e4060986f
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2020-04-05 01:05:48 -0400

    Update maxSubArray.js

--- maxSubArray.js
+++ maxSubArray.js
@@ -12,3 +12,21 @@
  maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
  
  //Output: 6
+
+
+// This logic is wrong but passed all tests
+
+  function maxSubArray(nums) {
+   
+ let current = 0
+  let maxSum = nums[0]
+  nums.forEach((idx, num) => {
+    current = Math.max(idx, current + idx)
+    maxSum = Math.max(maxSum, current)
+  })
+  return maxSum
+};
+
+ maxSubArray([-4])
+ 
+ //Output:-4
